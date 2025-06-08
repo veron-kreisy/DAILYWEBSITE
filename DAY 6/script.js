aac92edf6c7a799fff99272e1ed1773c6b866e46
Sumer Patel <85951981+sumer173@users.noreply.github.com>
2025-06-08 16:21:17 +0530

    Update script.js
    Your show button is not working so I fixed it.

--- DAY 6/script.js
+++ DAY 6/script.js
@@ -20,11 +20,23 @@
 // console.log(maskEmail(email));
 
 
+// const togglePassword = document.getElementById("togglePassword");
+// const passwordInput = document.getElementById("password");
+
+// togglePassword.addEventListener("click", () => {
+//   const type = passwordInput.getAttribute("type") === "password" ? "text" : "password";
+//   passwordInput.setAttribute("type", type);
+//   togglePassword.textContent = type === "password" ? "Show" : "Hide";
+// });
+
+
+// this is the my javascript code
+
 const togglePassword = document.getElementById("togglePassword");
 const passwordInput = document.getElementById("password");
 
 togglePassword.addEventListener("click", () => {
-  const type = passwordInput.getAttribute("type") === "password" ? "text" : "password";
-  passwordInput.setAttribute("type", type);
-  togglePassword.textContent = type === "password" ? "Show" : "Hide";
+  const isPassword = passwordInput.type === "password";
+  passwordInput.type = isPassword ? "text" : "password";
+  togglePassword.textContent = isPassword ? "Hide" : "Show";
 });
